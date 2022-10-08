30ac8bcf707e30c1a315c29b434aa857c7c6cf67
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-10-08 15:16:55 +0500

    Carpentry

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,31 +7,62 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [jwt, setJwt] = useLocalState("", "jwt");
+  // const [jwt, setJwt] = useLocalState("", "jwt");
 
   const navigate = useNavigate();
 
-  function sendLoginRequest() {
+  const sendLoginRequest = () => {
+    if (email == "") {
+      alert("Email shouldn't be empty");
+      return;
+    } else if (password == "") {
+      alert("Password shouldn't be empty");
+      return;
+    }
+
     const reqBody = {
-      email: email,
-      password: password,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: email, password: password }),
     };
-    fetch("http://localhost:1234/loginCustomer", {
-      headers: "POST",
-      body: JSON.stringify(reqBody),
-    })
+
+    fetch("http://localhost:1234/loginCustomer", reqBody)
+      .then((res) => res.json())
       .then((res) => {
-        if (res.status === 200) return Promise.all([res.json(), res.headers]);
-        else return Promise.reject("invalid login attempt");
-      })
-      .then(([body, headers]) => {
-        setJwt(headers.get("authorization"));
-        navigate("/home");
+        if (res["toke"] != null) {
+          localStorage.setItem("token", res["token"]); //token
+          localStorage.setItem("email", res["email_details"]["email"]); //email
+          setEmail("");
+        } else {
+          alert(res["message"]);
+        }
       })
       .catch((err) => {
-        alert(err);
+        console.log(err);
       });
-  }
+  };
+
+  // function sendLoginRequest() {
+  //   const reqBody = {
+  //     email: email,
+  //     password: password,
+  //   };
+  //   fetch("http://localhost:1234/loginCustomer", {
+  //     headers: "POST",
+  //     body: JSON.stringify(reqBody),
+  //   })
+  //     .then((res) => {
+  //       if (res.status === 200) return Promise.all([res.json(), res.headers]);
+  //       else return Promise.reject("invalid login attempt");
+  //     })
+  //     .then(([body, headers]) => {
+  //       setJwt(headers.get("authorization"));
+  //       navigate("/home");
+  //     })
+  // .catch((err) => {
+  //   alert(err);
+  // });
+  // }
 
   return (
     <>
