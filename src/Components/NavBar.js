98e62dc99ea13454a55c9daecc4899f05bb7047b
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-10-10 22:58:05 +0500

    Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 import "../Style/Style.css";
 import logo from "../images/log.png";
 import { NavDropdown } from "react-bootstrap";
-
 import { Navbar } from "react-bootstrap";
+import { FaBars } from "react-icons/fa";
 
 function NavBar() {
   const [isMobile, setIsMobile] = useState(false);
@@ -20,27 +20,14 @@ function NavBar() {
           onClick={() => setIsMobile(false)}
         >
           <NavLink className="home" to="/">
-            {/* <i
-              className="zmdi zmdi-home zmdi-hc-lg"
-              style={{ marginLeft: "80%", marginTop: "45%" }}
-            ></i> */}
             <li>Home</li>
           </NavLink>
 
           <NavLink className="about" to="/about">
-            {/* <i
-              className="zmdi zmdi-accounts zmdi-hc-lg"
-              style={{ marginLeft: "80%", marginTop: "35%" }}
-            ></i> */}
             <li>About</li>
           </NavLink>
 
           <div className="service">
-            {/* using material design icons */}
-            {/* <i
-              className="fa-solid fa-people-carry-box fa-lg"
-              style={{ marginLeft: "35%", marginTop: "15%" }}
-            ></i> */}
             <NavDropdown
               title="Services"
               id="navbarScrollingDropdown"
@@ -80,24 +67,9 @@ function NavBar() {
           </div>
 
           <NavLink className="company" to="/company">
-            {/* <i
-              className="fa-solid fa-building-circle-check fa-lg"
-              style={{ marginLeft: "30%", marginTop: "30%" }}
-            ></i> */}
             <li>Company</li>
           </NavLink>
-
-          {/* <NavLink className="hire" to="/hire"> */}
-          {/* <UserOutlined /> */}
-          {/* <i class="fa-solid fa-user-check fa-lg"></i>
-            <li>Hire</li>
-          </NavLink> */}
-
           <NavLink className="contact" to="/contact">
-            {/* <i
-              className="fa-regular fa-address-card fa-lg"
-              style={{ marginLeft: "20%", marginTop: "37%" }}
-            ></i> */}
             <li>Contact</li>
           </NavLink>
 
@@ -140,38 +112,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-
-// function BasicExample() {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Container>
-//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#home">Home</Nav.Link>
-//             <Nav.Link href="#link">Link</Nav.Link>
-//             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.2">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action/3.4">
-//                 Separated link
-//               </NavDropdown.Item>
-//             </NavDropdown>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default BasicExample;
