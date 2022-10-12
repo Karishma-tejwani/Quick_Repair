160407a9a7ce275bb2196d449f71b363253a7a56
Karishma-tejwani <karishmatejwani9@gmail.com>
2022-10-13 02:34:58 +0500

    Orders

--- src/Dashboard/Orders.js
+++ src/Dashboard/Orders.js
@@ -24,6 +24,7 @@ const makeStyle = (status) => {
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const { id } = useParams();
+  console.log(id);
 
   useEffect(() => {
     fetch(`http://localhost:1234/getOrdersByAdminId?id=${id}`, {
@@ -36,48 +37,88 @@ const Orders = () => {
   }, []);
 
   return (
-    <div className="Dashboard">
-      <div className="DashGlass">
-        <Sidebar />
+    <>
+      <div className="Dashboard">
+        <div className="DashGlass">
+          <Sidebar />
 
-        <div className="Order">
-          <Table striped bordered hover variant="light" className="table-css">
-            <thead>
-              <tr>
-                <th>OrderId</th>
-                <th>UserName</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>ServiceName</th>
-                <th>City</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((itm, i) => {
-                return (
-                  <>
-                    <tr key={i}>
-                      <td>{itm.OrderId}</td>
-                      <td>{itm.UserName}</td>
-                      <td>{itm.Email}</td>
-                      <td>{itm.Phone}</td>
-                      <td>{itm.ServiceName}</td>
-                      <td>{itm.City}</td>
-                      <td>
-                        <span className="status" style={makeStyle(itm.status)}>
-                          {itm.status}
-                        </span>
-                      </td>
-                    </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </Table>
+          <div className="Row">
+            <h2>Customers List</h2>
+            <Table striped bordered hover variant className="table-css">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Address</th>
+                  <th>City</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((itm, i) => {
+                  return (
+                    <>
+                      <tr key={i}>
+                        <td>{itm.id}</td>
+                        <td>{itm.type}</td>
+                        <td>{itm.date}</td>
+                        <td>{itm.charges}</td>
+                        <td>{itm.status}</td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* <div className="Dashboard">
+        <div className="DashGlass">
+          <Sidebar />
+
+          <div className="Order">
+            <Table striped bordered hover variant="light" className="table-css">
+              <thead>
+                <tr>
+                  <th>OrderId</th>
+                  <th>Service Type</th>
+                  <th>Date</th>
+                  <th>Charges</th>
+                  <th>Admin</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((itm, i) => {
+                  return (
+                    <>
+                      <tr key={i}>
+                        <td>{itm.id}</td>
+                        <td>{itm.type}</td>
+                        <td>{itm.date}</td>
+                        <td>{itm.charges}</td>
+                        <td>{itm.adminName}</td>
+                        <td>{itm.status}</td>
+                        <td>
+                          <span
+                            className="status"
+                            style={makeStyle(itm.status)}
+                          >
+                            {itm.status}
+                          </span>
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 };
 
